50623be57effed8033e230953bde7b26e3bda214
hilarious-state-5373 <jacob.lam.work@gmail.com>
2019-02-27 18:38:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToBeginningOfArray(array,element){
    return array
  }
  
- function accessElementInArray(array,element){
-  return array[element]
+ function accessElementInArray(array,index){
+  return array[index]
  }
