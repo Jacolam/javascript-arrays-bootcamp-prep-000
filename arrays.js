af5c5f8e8a56e2c4c7037c46c7fcbfd843b53d9c
hilarious-state-5373 <jacob.lam.work@gmail.com>
2019-02-27 18:18:15 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+ var chocolateBars = ["snickers", "hungred grand", "kitkat", "skittles"]
+ 
+ 
+ function addElementToBeginningOfArray(array, element){
+   return [array,...element]
+ }
+
+
+ function destructivelyAddElementToBeginningOfArray(array, element) {
+    array.unshift(element)
+    return array }
